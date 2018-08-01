203614e5876161316ebd512ed23a146d5a5beca7
Kuba Krzemiński <jakubkrzem@gmail.com>
2018-08-01 22:17:00 +0200

    Wstępna wersja metody odbierającej dane w bibliotele rest

--- ClientApp/ClientApp/Form1.cs
+++ ClientApp/ClientApp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using ProjectClasses.Classes;
+using RESTLib;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,17 +19,21 @@ namespace ClientApp
     {
         BindingList<User> gridList = new BindingList<User>();
         private BindingSource bindingSource1 = new BindingSource();
+        Receiver receiver;
+        
         public Form1()
         {
             InitializeComponent();
+            receiver = new Receiver("http://localhost:51412/api/values");
         }
 
         private void btnGetUsers_Click(object sender, EventArgs e)
         {
             User[] user;
+            /*
             string url = string.Format("http://localhost:51412/api/values/user/{0}", textBox1.Text.ToString()); //JKRze - porpawic 
 
-            using (var w = new WebClient())
+            using (WebClient w = new WebClient())
             {
                 w.Encoding = Encoding.UTF8;
                 var json_data = string.Empty;
@@ -44,15 +49,33 @@ namespace ClientApp
                 lblId.Text = user.Id.ToString();
                 lblLastName.Text = user.LastName;
                 lblName.Text = user.Name;
-                lblRoom.Text = user.Room.Code;*/
+                lblRoom.Text = user.Room.Code;
                 bindingSource1.Clear();
                 foreach (User u in user)
                     gridList.Add(u);
-                bindingSource1 = new BindingSource(gridList, null);
+                bindingSource1 = new BindingSource(gridList, null);*/
+
+            string[] parameters = new string[0];
+            bool isArray;
+            if (textBox1.Text.ToString() != string.Empty)
+            {
+                parameters = new string[1] { textBox1.Text.ToString() };
+                isArray = false;
+            }
+            else
+                isArray = true;
 
 
+            user = receiver.ReceiveData<User>("user", parameters, isArray);
+
+            if (user[0] == null)
+            {
+                MessageBox.Show("brak danych");
+            }
+            else
+            {
                 tableModel1.Rows.Clear();
-                for (int i  = 0; i < user.Count(); i++)
+                for (int i = 0; i < user.Count(); i++)
                 {
                     tableModel1.Rows.Add(new XPTable.Models.Row());
                     tableModel1.Rows[i].Cells.Add(new XPTable.Models.Cell(user[i].Id.ToString()));
@@ -61,6 +84,7 @@ namespace ClientApp
                     tableModel1.Rows[i].Cells.Add(new XPTable.Models.Cell(user[i].LastName));
 
                 }
+
             }
 
 
---
+++ ClientApp/RESTLib/Receiver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTLib
+{
+    public class Receiver
+    {
+        public string Address { get; }
+
+
+        public Receiver(string adress)
+        {
+            Address = adress;
+
+        }
+
+        public T[] ReceiveData<T>(string method, string[] parameters, bool isArray)
+        {
+            List<T> values;
+            T value;
+            String url = Address + "/" + method.ToLower() + "/";
+
+            foreach (string param in parameters)
+                url = url + param + "/";
+            using (WebClient w = new WebClient())
+            {
+                w.Encoding = Encoding.UTF8;
+                var json_data = string.Empty;
+                try
+                {
+                    json_data = w.DownloadString(url);
+                }
+                catch (Exception) { }
+                if (isArray)
+                {
+                    values = ((T[])Newtonsoft.Json.JsonConvert.DeserializeObject<T[]>(json_data)).ToList();
+                    return values.ToArray();
+                }
+                else
+                {
+                    value = ((T)Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json_data));
+                    return new T[1] { value };
+                }
+            }
+        }
+
+
+    }
+}
